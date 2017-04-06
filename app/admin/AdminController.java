1ee49db5e009eddb5b26d27c3f1b0be70b07ad35
fdschwyter <fab.schwyter@gmail.com>
2017-04-06 12:42:05 +0200

    Update Function

--- app/admin/AdminController.java
+++ app/admin/AdminController.java
@@ -4,15 +4,17 @@ import play.mvc.Controller;
 import play.mvc.Result;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Map;
 
 /**
  * Created by Fabian on 06.04.17.
  */
 public class AdminController extends Controller {
 
-    public Result executeUpdate(){
+    public Result executeUpdate2(){
         StringBuffer output = new StringBuffer();
 
         Process process;
@@ -33,5 +35,18 @@ public class AdminController extends Controller {
         }
 
         return ok(output.toString());
+    }
+
+    public Result executeUpdate(){
+
+        ProcessBuilder pb = new ProcessBuilder("conf/update.sh");
+
+        try {
+            Process p = pb.start();
+        } catch (IOException e) {
+            return ok(e.getMessage());
+        }
+
+        return ok("Update");
     }
 }
