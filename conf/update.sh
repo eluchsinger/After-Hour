9da7b8174f7e756da16deb7564116a8b634343fb
fdschwyter <fab.schwyter@gmail.com>
2017-04-06 14:32:22 +0200

    Update.sh logging with time

--- conf/update.sh
+++ conf/update.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo ---------------- >> update.log
-echo date '+%A %W %Y %X' >> update.log
+date '+%A %W %Y %X' >> update.log
 git pull git@github.com:eluchsinger/After-Hour.git >> update.log
