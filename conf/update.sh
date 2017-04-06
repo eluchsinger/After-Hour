aa5e5498bd1702c6fd5aae88edbccc38b4e902ea
fdschwyter <fab.schwyter@gmail.com>
2017-04-06 14:28:01 +0200

    Update.sh logging

--- conf/update.sh
+++ conf/update.sh
@@ -1,1 +1,3 @@
-git pull git@github.com:eluchsinger/After-Hour.git > update.log
+#!/usr/bin/env bash
+echo ---------------- >> update.log
+git pull git@github.com:eluchsinger/After-Hour.git >> update.log
