108dba4ca9226b446dc92584f33067af520af2cb
fdschwyter <fab.schwyter@gmail.com>
2017-05-10 13:30:55 +0200

    Implemented logic to buy ticket.
    User can not be null,
    TicketCategory can not be null and has correct buy time.
    User can only buy one Ticket.

--- app/logic/sales/SalesLogicImpl.java
+++ app/logic/sales/SalesLogicImpl.java
@@ -5,8 +5,11 @@ import dal.users.UsersRepository;
 import models.events.TicketCategory;
 import models.tickets.Ticket;
 import models.users.User;
+import org.joda.time.DateTime;
 
 import javax.inject.Inject;
+import java.util.Date;
+import java.util.Optional;
 
 /**
  * Created by Fabian on 08.04.17.
@@ -36,10 +39,37 @@ public class SalesLogicImpl implements SalesLogic{
     public Ticket buyTicket(final Integer userId, final Integer ticketCategoryId) {
         TicketCategory ticketCategory = this.ticketRepository
                 .getTicketCategoryById(ticketCategoryId);
+
         User buyingUser = this.usersRepository.getUserById(userId);
         Ticket soldTicket = ticketCategory.sellTicket(buyingUser);
-        this.ticketRepository.persistTicket(soldTicket);
 
-        return soldTicket;
+        if (validateUser(buyingUser) && validateTicket(ticketCategory) && validateTicketBought(ticketCategoryId, userId)){
+            this.ticketRepository.persistTicket(soldTicket);
+
+            return soldTicket;
+        }
+        return null;
     }
+
+    public boolean validateUser(final User buyingUser){
+        return buyingUser != null;
+    }
+
+    public boolean validateTicket(final TicketCategory ticketCategory){
+        return ticketCategory != null
+                && ticketCategory.getEndAvailability().before(new Date())
+                && ticketCategory.getStartAvailability().after(new Date());
+    }
+
+    public boolean validateTicketBought(final Integer ticketCategoryId, final Integer userId){
+        Optional<Ticket> optionalTicket = this.usersRepository.getUserById(1).getTickets().stream()
+                .filter(x -> x.getTicketCategory().getId().equals(ticketCategoryId))
+                .findFirst();
+        if (!optionalTicket.isPresent()){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
